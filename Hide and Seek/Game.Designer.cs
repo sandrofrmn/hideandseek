aa07058ed373b16253001ab99d9b6571acd51780
sandrofrmn <sandro.offermans@icloud.com>
2020-06-11 13:56:09 +0200

    v1.6
    - buttons added
    - ...

--- Hide and Seek/Game.Designer.cs
+++ Hide and Seek/Game.Designer.cs
@@ -38,7 +38,23 @@
             this.timerRoom = new System.Windows.Forms.Timer(this.components);
             this.timerHall = new System.Windows.Forms.Timer(this.components);
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Log = new System.Windows.Forms.DataGridView();
+            this.verstoppertjeDatabaseDataSet = new Hide_and_Seek.VerstoppertjeDatabaseDataSet();
+            this.verstopperLogBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.verstopperLogTableAdapter = new Hide_and_Seek.VerstoppertjeDatabaseDataSetTableAdapters.VerstopperLogTableAdapter();
+            this.hiderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roomDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountOfSecondsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonBathroom = new System.Windows.Forms.Button();
+            this.buttonBedroom = new System.Windows.Forms.Button();
+            this.buttonHallway = new System.Windows.Forms.Button();
+            this.buttonKitchen = new System.Windows.Forms.Button();
+            this.buttonLivingroom = new System.Windows.Forms.Button();
+            this.buttonToilet = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Log)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verstoppertjeDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verstopperLogBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -76,7 +92,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Hide_and_Seek.Properties.Resources.Floorplan_with_names;
-            this.pictureBox1.Location = new System.Drawing.Point(215, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(78, 30);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(684, 423);
@@ -97,7 +113,7 @@
             // 
             this.webBrowser1.AllowNavigation = false;
             this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
-            this.webBrowser1.Location = new System.Drawing.Point(215, 30);
+            this.webBrowser1.Location = new System.Drawing.Point(78, 30);
             this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
@@ -108,11 +124,133 @@
             this.webBrowser1.Url = new System.Uri("http://localhost:8080/#/Floorplans", System.UriKind.Absolute);
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
+            // Log
+            // 
+            this.Log.AutoGenerateColumns = false;
+            this.Log.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Log.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.hiderDataGridViewTextBoxColumn,
+            this.roomDataGridViewTextBoxColumn,
+            this.amountOfSecondsDataGridViewTextBoxColumn});
+            this.Log.DataSource = this.verstopperLogBindingSource;
+            this.Log.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Log.Location = new System.Drawing.Point(786, 94);
+            this.Log.Name = "Log";
+            this.Log.RowHeadersWidth = 51;
+            this.Log.RowTemplate.Height = 24;
+            this.Log.Size = new System.Drawing.Size(586, 302);
+            this.Log.TabIndex = 7;
+            // 
+            // verstoppertjeDatabaseDataSet
+            // 
+            this.verstoppertjeDatabaseDataSet.DataSetName = "VerstoppertjeDatabaseDataSet";
+            this.verstoppertjeDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // verstopperLogBindingSource
+            // 
+            this.verstopperLogBindingSource.DataMember = "VerstopperLog";
+            this.verstopperLogBindingSource.DataSource = this.verstoppertjeDatabaseDataSet;
+            // 
+            // verstopperLogTableAdapter
+            // 
+            this.verstopperLogTableAdapter.ClearBeforeFill = true;
+            // 
+            // hiderDataGridViewTextBoxColumn
+            // 
+            this.hiderDataGridViewTextBoxColumn.DataPropertyName = "Hider";
+            this.hiderDataGridViewTextBoxColumn.HeaderText = "Hider";
+            this.hiderDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hiderDataGridViewTextBoxColumn.Name = "hiderDataGridViewTextBoxColumn";
+            this.hiderDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // roomDataGridViewTextBoxColumn
+            // 
+            this.roomDataGridViewTextBoxColumn.DataPropertyName = "Room";
+            this.roomDataGridViewTextBoxColumn.HeaderText = "Room";
+            this.roomDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.roomDataGridViewTextBoxColumn.Name = "roomDataGridViewTextBoxColumn";
+            this.roomDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // amountOfSecondsDataGridViewTextBoxColumn
+            // 
+            this.amountOfSecondsDataGridViewTextBoxColumn.DataPropertyName = "AmountOfSeconds";
+            this.amountOfSecondsDataGridViewTextBoxColumn.HeaderText = "AmountOfSeconds";
+            this.amountOfSecondsDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.amountOfSecondsDataGridViewTextBoxColumn.Name = "amountOfSecondsDataGridViewTextBoxColumn";
+            this.amountOfSecondsDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // buttonBathroom
+            // 
+            this.buttonBathroom.Location = new System.Drawing.Point(192, 123);
+            this.buttonBathroom.Name = "buttonBathroom";
+            this.buttonBathroom.Size = new System.Drawing.Size(109, 23);
+            this.buttonBathroom.TabIndex = 8;
+            this.buttonBathroom.Text = "Bathroom";
+            this.buttonBathroom.UseVisualStyleBackColor = true;
+            this.buttonBathroom.Visible = false;
+            // 
+            // buttonBedroom
+            // 
+            this.buttonBedroom.Location = new System.Drawing.Point(192, 283);
+            this.buttonBedroom.Name = "buttonBedroom";
+            this.buttonBedroom.Size = new System.Drawing.Size(109, 28);
+            this.buttonBedroom.TabIndex = 9;
+            this.buttonBedroom.Text = "Bedroom";
+            this.buttonBedroom.UseVisualStyleBackColor = true;
+            this.buttonBedroom.Visible = false;
+            // 
+            // buttonHallway
+            // 
+            this.buttonHallway.Location = new System.Drawing.Point(410, 170);
+            this.buttonHallway.Name = "buttonHallway";
+            this.buttonHallway.Size = new System.Drawing.Size(75, 27);
+            this.buttonHallway.TabIndex = 10;
+            this.buttonHallway.Text = "Hallway";
+            this.buttonHallway.UseVisualStyleBackColor = true;
+            this.buttonHallway.Visible = false;
+            // 
+            // buttonKitchen
+            // 
+            this.buttonKitchen.Location = new System.Drawing.Point(543, 300);
+            this.buttonKitchen.Name = "buttonKitchen";
+            this.buttonKitchen.Size = new System.Drawing.Size(117, 28);
+            this.buttonKitchen.TabIndex = 11;
+            this.buttonKitchen.Text = "Kitchen";
+            this.buttonKitchen.UseVisualStyleBackColor = true;
+            this.buttonKitchen.Visible = false;
+            // 
+            // buttonLivingroom
+            // 
+            this.buttonLivingroom.Location = new System.Drawing.Point(543, 141);
+            this.buttonLivingroom.Name = "buttonLivingroom";
+            this.buttonLivingroom.Size = new System.Drawing.Size(117, 30);
+            this.buttonLivingroom.TabIndex = 12;
+            this.buttonLivingroom.Text = "Livingroom";
+            this.buttonLivingroom.UseVisualStyleBackColor = true;
+            this.buttonLivingroom.Visible = false;
+            // 
+            // buttonToilet
+            // 
+            this.buttonToilet.Location = new System.Drawing.Point(516, 249);
+            this.buttonToilet.Name = "buttonToilet";
+            this.buttonToilet.Size = new System.Drawing.Size(75, 23);
+            this.buttonToilet.TabIndex = 13;
+            this.buttonToilet.Text = "Toilet";
+            this.buttonToilet.UseVisualStyleBackColor = true;
+            this.buttonToilet.Visible = false;
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1093, 672);
+            this.ClientSize = new System.Drawing.Size(1401, 672);
+            this.Controls.Add(this.buttonToilet);
+            this.Controls.Add(this.buttonLivingroom);
+            this.Controls.Add(this.buttonKitchen);
+            this.Controls.Add(this.buttonHallway);
+            this.Controls.Add(this.buttonBedroom);
+            this.Controls.Add(this.buttonBathroom);
+            this.Controls.Add(this.Log);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.roomName);
             this.Controls.Add(this.pictureBox1);
@@ -124,6 +262,9 @@
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Log)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verstoppertjeDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verstopperLogBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,5 +281,18 @@
         private System.Windows.Forms.Timer timerRoom;
         private System.Windows.Forms.Timer timerHall;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.DataGridView Log;
+        private VerstoppertjeDatabaseDataSet verstoppertjeDatabaseDataSet;
+        private System.Windows.Forms.BindingSource verstopperLogBindingSource;
+        private VerstoppertjeDatabaseDataSetTableAdapters.VerstopperLogTableAdapter verstopperLogTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hiderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roomDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountOfSecondsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonBathroom;
+        private System.Windows.Forms.Button buttonBedroom;
+        private System.Windows.Forms.Button buttonHallway;
+        private System.Windows.Forms.Button buttonKitchen;
+        private System.Windows.Forms.Button buttonLivingroom;
+        private System.Windows.Forms.Button buttonToilet;
     }
 }
--- Hide and Seek/Game.cs
+++ Hide and Seek/Game.cs
@@ -19,17 +19,18 @@ namespace Hide_and_Seek
         public int allowedMinutes;
         public string setDifficulty;
         public bool start = true;
+        public bool preRound = true;
+        public bool lockDifficulty = false;
         List<string> Rooms = new List<string>() { "Hallway", "Bedroom", "Toilet", "Bathroom", "Livingroom", "Kitchen" };
         List<int> MotionSensors = new List<int>() { 3, 9, 10, 11, 12, 13 };
+        public int count = 0;
         Random random = new Random();
         DAL dal = new DAL();
 
-        //TODO (DONE): create game into another table sql, so every game has its own id. UPDATE: Fixed by clearing records at the start of every game.
         //TODO: fix bedroom -> bathroom and bathroom -> bedroom where it takes currently 4 to 8 seconds (timerRoom) instead of 7 to 45 (timerHall)
-        //TODO: If hallway is logged, into the database, it logs twice with the same AmountOfSeconds
-        //TODO: Create room buttons for the Seeker (Create the seeker system in a new form that starts af the pre round??)
+        //TODO: If hallway is logged into the database, it logs multiple times with the same AmountOfSeconds
         //TODO: In the seeker system a log from the pre round will be showed
-        //TODO: The webpage must be cropped for a nicer look and it has to refresh automatically
+        //TODO: The webpage must be cropped for a nicer look
         
 
         public Game(int inputMinutes, string inputDifficulty)
@@ -39,23 +40,24 @@ namespace Hide_and_Seek
             setDifficulty = inputDifficulty;
             timer1.Start();
             dal.DeleteRecords();
-
         }
 
         private void Game_Load(object sender, EventArgs e)
         {
+            this.verstopperLogTableAdapter.Fill(this.verstoppertjeDatabaseDataSet.VerstopperLog);
             if (start)
             {
-
-
                 roomName.Text = "Hallway";
                 dal.TurnGroupOff(2);
                 dal.TurnOn(3);
+                webBrowser1.Refresh();
+                
                 start = false;
             }
             timerRoom.Start();
             timerHall.Start();
-            timerRoom.Interval = random.Next(4000, 8000);
+            timerRoom.Interval = random.Next(2000, 3000);
+            dal.WriteToDomDb(1, roomName.Text, Convert.ToInt32(timerRoom.Interval / 1000));
 
             dal.TurnOff(3);
             dal.TurnOff(9);
@@ -65,53 +67,91 @@ namespace Hide_and_Seek
 
         public void OnTimerEvent(object source, EventArgs e)
         {
+            count = count + 1;
+            Console.WriteLine(count);
+            Log.Update();
+            Log.Refresh();
+            this.verstopperLogTableAdapter.Fill(this.verstoppertjeDatabaseDataSet.VerstopperLog);
+            
             dal.TurnGroupOff(2);
             int current_room = random.Next(0, Rooms.Count());
-            dal.WriteToDomDb(4, roomName.Text, Convert.ToInt32(timerRoom.Interval / 1000));
             roomName.Text = Rooms.ElementAt(current_room);
-
+            
+            
             int sensor = MotionSensors.ElementAt(current_room);
-
             dal.TurnOn(sensor);
+
+            webBrowser1.Refresh();
+            
             timerRoom.Stop();
 
             timerHall.Start();
-            timerHall.Interval = random.Next(7000, 45000);
-
+            timerHall.Interval = random.Next(3000, 5000);
+            
+                        
             timerHall.Tick += new EventHandler(TimerHallEvent);
         }
 
         public void TimerHallEvent(object source, EventArgs e)
         {
-            dal.WriteToDomDb(4, roomName.Text, Convert.ToInt32(timerHall.Interval / 1000));
+
+            dal.WriteToDomDb(1, roomName.Text, Convert.ToInt32(timerHall.Interval / 1000)+1);
+            Log.Update();
+            Log.Refresh();
+            this.verstopperLogTableAdapter.Fill(this.verstoppertjeDatabaseDataSet.VerstopperLog);
             dal.TurnGroupOff(2);
+            /*
             int number = random.Next(0, 1);
             if ((number == 0 && roomName.Text == "Bedroom") || (number == 0 && roomName.Text == "Bathroom"))
             {
                 if (roomName.Text == "Bedroom")
                 {
                     roomName.Text = "Bathroom";
+                    
                     dal.TurnOn(11);
                     dal.TurnOff(9);
+                    webBrowser1.Refresh();
+                    Log.Update();
+                    Log.Refresh();
                     start = true;
                 }
                 else
                 {
                     roomName.Text = "Bedroom";
+                    
                     dal.TurnOn(9);
                     dal.TurnOff(11);
+                    webBrowser1.Refresh();
+                    
+                    Log.Update();
+                    Log.Refresh();
+                    this.verstopperLogTableAdapter.Fill(this.verstoppertjeDatabaseDataSet.VerstopperLog);
                     start = true;
                 }
             }
             else
             {
                 roomName.Text = "Hallway";
+                
                 dal.TurnOn(3);
+                webBrowser1.Refresh();
+                Log.Update();
+                Log.Refresh();
             }
+            */
+
 
             timerHall.Stop();
             timerRoom.Start();
+            timerRoom.Interval = random.Next(4000, 6000);
 
+            roomName.Text = "test kamer";
+            dal.WriteToDomDb(1, roomName.Text, Convert.ToInt32(timerHall.Interval / 1000) + 1);
+
+
+            Log.Update();
+            Log.Refresh();
+            this.verstopperLogTableAdapter.Fill(this.verstoppertjeDatabaseDataSet.VerstopperLog);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -136,26 +176,54 @@ namespace Hide_and_Seek
                 _seconds = 0;
             }
 
-            if (setDifficulty == "Easy")
+            if (lockDifficulty == false)
             {
-                setDifficulty = "0";
+                if (setDifficulty == "Easy")
+                {
+                    setDifficulty = "0";
+                }
+                else
+                {
+                    setDifficulty = "1";
+                }
+                lockDifficulty = true;
             }
-            else
-            {
-                setDifficulty = "1";
-            }
+
 
             if (_minutes == (allowedMinutes - Convert.ToInt32(setDifficulty)) && _seconds == 0)
             {
                 roomName.Visible = false;
+                if (setDifficulty == "1")
+                {
+                    webBrowser1.Visible = false;
+                }
             }
 
             if (_minutes >= allowedMinutes)
             {
                 timer1.Stop();
-                timerHall.Stop();
-                timerRoom.Stop();
-                time_elapsed.Text = allowedMinutes + ":00";
+                if (preRound)
+                {
+                    preRound = false;
+                    _minutes = 0;
+                    _seconds = 0;
+                    timer1.Start();
+                    webBrowser1.Visible = false;
+                    buttonBathroom.Visible = true;
+                    buttonBedroom.Visible = true;
+                    buttonHallway.Visible = true;
+                    buttonKitchen.Visible = true;
+                    buttonLivingroom.Visible = true;
+                    buttonToilet.Visible = true;
+                } 
+                else
+                {
+                    timerHall.Stop();
+                    timerRoom.Stop();
+                }
+                
+
+                //time_elapsed.Text = allowedMinutes + ":00";
                 dal.TurnOn(14);
             }
         }
